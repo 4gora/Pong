0305ca53fc89bc1e0edc4794aea1826e3605e5ff
4gora <rosarioalessandro03@gmail.com>
2024-03-05 22:51:39 -0300

    Bola + Grid
    - Criação da bola e seu movimento.
    - Adicionado método show_grid() em utils.py.

--- Cod/pong.py
+++ Cod/pong.py
@@ -7,6 +7,7 @@ pygame.init()
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+SCREEN_CENTER = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 clock = pygame.time.Clock()
 
 
@@ -14,8 +15,13 @@ clock = pygame.time.Clock()
 pygame.display.set_caption("Pong")
 font = pygame.font.Font("font/Pixeltype.ttf", 100)
 title_surface = font.render("Pong", None, "WHITE")
+rect_title = title_surface.get_rect(center=(SCREEN_CENTER[0], 100))
+
 
 fps_toggle = False
+grid_toggle = False
+
+# ---------------------------------------------------------------------------------
 
 # DEFININDO TAMANHO DOS RETÂNGULOS (JOGADORES)
 ret_width, ret_height = 20, 150
@@ -30,6 +36,11 @@ adversario_pos = pygame.Vector2(700, 250)
 adversario_vel = 4
 adversario = pygame.Rect(adversario_pos.x, adversario_pos.y, ret_width, ret_height)
 
+# DEFININDO BOLA
+bola_pos = pygame.Vector2(SCREEN_CENTER)
+bola_vel = pygame.Vector2(4, 4)
+bola_r = 20
+
 
 #  LOOP PRINCIPAL
 while True:
@@ -40,9 +51,20 @@ while True:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_f:
                 fps_toggle = not fps_toggle
+            elif event.key == pygame.K_g:
+                grid_toggle = not grid_toggle
 
     SCREEN.fill("BLACK")
-    SCREEN.blit(title_surface, (350, 100))
+    SCREEN.blit(title_surface, rect_title)
+
+    # MOSTRANDO JOGADORES
+    adversario = pygame.Rect(adversario_pos.x, adversario_pos.y, ret_width, ret_height)
+    jogador = pygame.Rect(jogador_pos.x, jogador_pos.y, ret_width, ret_height)
+    pygame.draw.rect(SCREEN, "WHITE", jogador)
+    pygame.draw.rect(SCREEN, "WHITE", adversario)
+
+    # BOLA
+    pygame.draw.circle(SCREEN, "white", bola_pos, bola_r)
 
     # DEFININDO MOVIMENTO DO ADVERSÁRIO
     adversario_pos.y += adversario_vel
@@ -51,11 +73,14 @@ while True:
     elif adversario.top <= 0:
         adversario_vel = abs(adversario_vel)
 
-    # MOSTRANDO JOGADORES
-    adversario = pygame.Rect(adversario_pos.x, adversario_pos.y, ret_width, ret_height)
-    jogador = pygame.Rect(jogador_pos.x, jogador_pos.y, ret_width, ret_height)
-    pygame.draw.rect(SCREEN, "WHITE", jogador)
-    pygame.draw.rect(SCREEN, "WHITE", adversario)
+    # DEFININDO MOVIMENTO DA BOLA
+    bola_pos.x += bola_vel.x
+    bola_pos.y += bola_vel.y
+
+    if bola_pos.x - bola_r <= 0 or bola_pos.x + bola_r >= SCREEN_WIDTH:
+        bola_vel.x = -bola_vel.x
+    if bola_pos.y - bola_r <= 0 or bola_pos.y + bola_r >= SCREEN_HEIGHT:
+        bola_vel.y = -bola_vel.y
 
     # MOVIMENTO DO JOGADOR
     keys = pygame.key.get_pressed()
@@ -70,8 +95,11 @@ while True:
             jogador_pos.y, SCREEN_HEIGHT - jogador.height
         )  # limita o movimento para baixo com a função min()
 
+    # TOGGLE BINDS
     if fps_toggle:
         utils.show_fps(clock, SCREEN)
+    if grid_toggle:
+        utils.show_grid(SCREEN, SCREEN_WIDTH)
 
     pygame.display.update()
     clock.tick(60)
--- Cod/utils.py
+++ Cod/utils.py
@@ -7,3 +7,11 @@ def show_fps(clock, SCREEN):
     info_font = pygame.font.Font("font/Pixeltype.ttf", 30)
     fps_info = info_font.render(str(int(clock.get_fps())), None, "green")
     SCREEN.blit(fps_info, (0, 0))
+
+
+# FUNÇÃO PARA MOSTRAR GRID
+
+def show_grid(SCREEN, SCREEN_WIDTH):
+    for x in range(0, SCREEN_WIDTH, 50):
+        pygame.draw.line(SCREEN, 'RED', (1, x), (SCREEN_WIDTH, x), 1)
+        pygame.draw.line(SCREEN, 'RED', (x, 1), (x, SCREEN_WIDTH), 1)
