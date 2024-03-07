3fa20d39cf92fdafcae6b4a3d8a2aa1dcbcb7ee4
4gora <rosarioalessandro03@gmail.com>
2024-03-06 21:30:46 -0300

    Reset + show caption
    - Implementado reset do jogo e legenda de binding.
    - Corrigido posição do adversário.
    - Alterado tamanho da bola e velocidade dos objetos.

--- Cod/pong.py
+++ Cod/pong.py
@@ -20,26 +20,30 @@ rect_title = title_surface.get_rect(center=(SCREEN_CENTER[0], 100))
 
 fps_toggle = False
 grid_toggle = False
+game_running = False
+caption_toggle = False
 
 # ---------------------------------------------------------------------------------
+
+vel = 8
 
 # DEFININDO TAMANHO DOS RETÂNGULOS (JOGADORES)
 ret_width, ret_height = 20, 150
 
 # DEFININDO JOGADOR
 jogador_pos = pygame.Vector2(50, 250)
-jogador_vel = 4
+jogador_vel = vel
 jogador = pygame.Rect(jogador_pos.x, jogador_pos.y, ret_width, ret_height)
 
 # DEFININDO ADVERSÁRIO
-adversario_pos = pygame.Vector2(700, 250)
-adversario_vel = 4
+adversario_pos = pygame.Vector2(SCREEN_WIDTH - 70, 250)
+adversario_vel = vel
 adversario = pygame.Rect(adversario_pos.x, adversario_pos.y, ret_width, ret_height)
 
 # DEFININDO BOLA
 bola_pos = pygame.Vector2(SCREEN_CENTER)
-bola_vel = pygame.Vector2(4, 4)
-bola_r = 20
+bola_vel = pygame.Vector2(vel, vel)
+bola_r = 15
 
 
 #  LOOP PRINCIPAL
@@ -53,6 +57,15 @@ while True:
                 fps_toggle = not fps_toggle
             elif event.key == pygame.K_g:
                 grid_toggle = not grid_toggle
+            elif event.key == pygame.K_p:
+                game_running = not game_running
+            elif event.key == pygame.K_r:
+                game_running = False
+                jogador_pos, adversario_pos, bola_pos, bola_vel = utils.reset(
+                    SCREEN_CENTER, SCREEN_WIDTH, vel
+                )
+            elif event.key == pygame.K_i:
+                caption_toggle = not caption_toggle
 
     SCREEN.fill("BLACK")
     SCREEN.blit(title_surface, rect_title)
@@ -66,40 +79,43 @@ while True:
     # BOLA
     pygame.draw.circle(SCREEN, "white", bola_pos, bola_r)
 
-    # DEFININDO MOVIMENTO DO ADVERSÁRIO
-    adversario_pos.y += adversario_vel
-    if adversario.bottom >= SCREEN_HEIGHT:
-        adversario_vel = -abs(adversario_vel)
-    elif adversario.top <= 0:
-        adversario_vel = abs(adversario_vel)
+    # PLAY / PAUSE
+    if game_running:
+        # DEFININDO MOVIMENTO DO ADVERSÁRIO
+        adversario_pos.y += adversario_vel
+        if adversario.bottom >= SCREEN_HEIGHT:
+            adversario_vel = -abs(adversario_vel)
+        elif adversario.top <= 0:
+            adversario_vel = abs(adversario_vel)
 
-    # DEFININDO MOVIMENTO DA BOLA
-    bola_pos.x += bola_vel.x
-    bola_pos.y += bola_vel.y
+        # DEFININDO MOVIMENTO DA BOLA
+        bola_pos += bola_vel
 
-    if bola_pos.x - bola_r <= 0 or bola_pos.x + bola_r >= SCREEN_WIDTH:
-        bola_vel.x = -bola_vel.x
-    if bola_pos.y - bola_r <= 0 or bola_pos.y + bola_r >= SCREEN_HEIGHT:
-        bola_vel.y = -bola_vel.y
+        if bola_pos.x - bola_r <= 0 or bola_pos.x + bola_r >= SCREEN_WIDTH:
+            bola_vel.x = -bola_vel.x
+        if bola_pos.y - bola_r <= 0 or bola_pos.y + bola_r >= SCREEN_HEIGHT:
+            bola_vel.y = -bola_vel.y
 
-    # MOVIMENTO DO JOGADOR
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        jogador_pos.y -= jogador_vel  # movimenta o jogador para cima
-        jogador_pos.y = max(
-            jogador_pos.y, 0
-        )  # limita o movimento para cima por meio da função max()
-    elif keys[pygame.K_s]:
-        jogador_pos.y += jogador_vel  # movimenta o jogador para baixo
-        jogador_pos.y = min(
-            jogador_pos.y, SCREEN_HEIGHT - jogador.height
-        )  # limita o movimento para baixo com a função min()
+        # MOVIMENTO DO JOGADOR
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            jogador_pos.y -= jogador_vel  # movimenta o jogador para cima
+            jogador_pos.y = max(
+                jogador_pos.y, 0
+            )  # limita o movimento para cima por meio da função max()
+        elif keys[pygame.K_s]:
+            jogador_pos.y += jogador_vel  # movimenta o jogador para baixo
+            jogador_pos.y = min(
+                jogador_pos.y, SCREEN_HEIGHT - jogador.height
+            )  # limita o movimento para baixo com a função min()
 
     # TOGGLE BINDS
     if fps_toggle:
         utils.show_fps(clock, SCREEN)
     if grid_toggle:
         utils.show_grid(SCREEN, SCREEN_WIDTH)
+    if caption_toggle:
+        utils.show_binding_caption(SCREEN, SCREEN_WIDTH)
 
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(75)
--- Cod/utils.py
+++ Cod/utils.py
@@ -1,7 +1,10 @@
 import pygame
 
+pygame.init()
 
-# FUNÇÃO PARA MOSTRAR FPS
+
+# MÉTODO PARA MOSTRAR FPS
+
 
 def show_fps(clock, SCREEN):
     info_font = pygame.font.Font("font/Pixeltype.ttf", 30)
@@ -9,9 +12,37 @@ def show_fps(clock, SCREEN):
     SCREEN.blit(fps_info, (0, 0))
 
 
-# FUNÇÃO PARA MOSTRAR GRID
+# MÉTODO PARA MOSTRAR GRID
+
 
 def show_grid(SCREEN, SCREEN_WIDTH):
     for x in range(0, SCREEN_WIDTH, 50):
-        pygame.draw.line(SCREEN, 'RED', (1, x), (SCREEN_WIDTH, x), 1)
-        pygame.draw.line(SCREEN, 'RED', (x, 1), (x, SCREEN_WIDTH), 1)
+        pygame.draw.line(SCREEN, "RED", (1, x), (SCREEN_WIDTH, x), 1)
+        pygame.draw.line(SCREEN, "RED", (x, 1), (x, SCREEN_WIDTH), 1)
+
+
+# MÉTODO PARA MOSTRAR LEGENDA DE BINDING
+
+
+def show_binding_caption(SCREEN, SCREEN_WIDTH):
+    caption_font = pygame.font.Font("font/Pixeltype.ttf", 20)
+    caption = caption_font.render(
+        "Play/ pause - P | Reset - R | Show FPS - F | Show grid - G | Exit menu - I",
+        True,
+        "green",
+    )
+
+    SCREEN.blit(caption, (SCREEN_WIDTH - caption.get_width() - 10, 10))
+
+
+# MÉTODO RESET
+
+
+def reset(SCREEN_CENTER, SCREEN_WIDTH, vel):
+
+    jogador_pos = pygame.Vector2(50, 250)
+    adversario_pos = pygame.Vector2(SCREEN_WIDTH - 70, 250)
+    bola_pos = pygame.Vector2(SCREEN_CENTER)
+    bola_vel = pygame.Vector2(vel, vel)
+
+    return jogador_pos, adversario_pos, bola_pos, bola_vel
