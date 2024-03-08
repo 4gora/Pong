6de0f51a04dad6be13b2f7a40022fd7f2e8ff284
4gora <rosarioalessandro03@gmail.com>
2024-03-08 17:32:42 -0300

    correção de sintaxe

--- Cod/utils.py
+++ Cod/utils.py
@@ -6,25 +6,25 @@ pygame.init()
 # FUNÇÃO PARA MOSTRAR FPS
 
 
-def show_fps(clock, SCREEN):
+def show_fps(clock, screen):
     info_font = pygame.font.Font("font/Pixeltype.ttf", 30)
     fps_info = info_font.render(str(int(clock.get_fps())), None, "green")
-    SCREEN.blit(fps_info, (0, 0))
+    screen.blit(fps_info, (0, 0))
 
 
 # FUNÇÃO PARA MOSTRAR GRID
 
 
-def show_grid(SCREEN, SCREEN_WIDTH):
-    for x in range(0, SCREEN_WIDTH, 50):
-        pygame.draw.line(SCREEN, "RED", (1, x), (SCREEN_WIDTH, x), 1)
-        pygame.draw.line(SCREEN, "RED", (x, 1), (x, SCREEN_WIDTH), 1)
+def show_grid(screen, screen_witdth):
+    for x in range(0, screen_witdth, 50):
+        pygame.draw.line(screen, "RED", (1, x), (screen_witdth, x), 1)
+        pygame.draw.line(screen, "RED", (x, 1), (x, screen_witdth), 1)
 
 
 # FUNÇÃO PARA MOSTRAR LEGENDA DE BINDING
 
 
-def show_binding_caption(SCREEN, SCREEN_WIDTH):
+def show_binding_caption(screen, screen_witdth):
     caption_font = pygame.font.Font("font/Pixeltype.ttf", 20)
     caption = caption_font.render(
         "Play/ pause - P | Reset - R | Show FPS - F | Show grid - G | Exit menu - I",
@@ -32,17 +32,17 @@ def show_binding_caption(SCREEN, SCREEN_WIDTH):
         "green",
     )
 
-    SCREEN.blit(caption, (SCREEN_WIDTH - caption.get_width() - 10, 10))
+    screen.blit(caption, (screen_witdth - caption.get_width() - 10, 10))
 
 
 # FUNÇÃO RESET
 
 
-def reset(SCREEN_CENTER, SCREEN_WIDTH, vel):
+def reset(screen_center, screen_witdth, vel):
 
     jogador_pos = pygame.Vector2(50, 250)
-    adversario_pos = pygame.Vector2(SCREEN_WIDTH - 70, 250)
-    bola_pos = pygame.Vector2(SCREEN_CENTER)
+    adversario_pos = pygame.Vector2(screen_witdth - 70, 250)
+    bola_pos = pygame.Vector2(screen_center)
     bola_vel = pygame.Vector2(vel, vel)
 
     return jogador_pos, adversario_pos, bola_pos, bola_vel
